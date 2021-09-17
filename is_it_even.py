ae8fea38f8abfca7460976bc5f1ae06ec4f8aa6e
abdullahi-7 <abdullahi.abdi@protonmail.com>
2021-09-17 14:16:02 +0300

    i can not see in screen correct answer

--- is_it_even.py
+++ is_it_even.py
@@ -7,5 +7,12 @@ def even():
     else:
         print(f"not even{var}")
     return vari
+def even_number():    
+    vari = []
+    var = int(input("pick a number"))
+    if var % 2 == 0:
+        vari.append(var)
+    else:
+        print(f"not even{var}")
+    return vari
     
-print(even())
