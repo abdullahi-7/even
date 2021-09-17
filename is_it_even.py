918ff995e04ec28903cf60ba9940ce9b957f78af
abdullahi-7 <abdullahi.abdi@protonmail.com>
2021-09-17 14:17:54 +0300

    they using if or else

--- is_it_even.py
+++ is_it_even.py
@@ -31,4 +31,12 @@ def even_numb():
     else:
         print(f"not even{var}")
     return vari
+def even_numb():    
+    vari = []
+    var = int(input("pick a number"))
+    if var % 2 == 0:
+        vari.append(var)
+    else:
+        print(f"not even{var}")
+    return vari
     
