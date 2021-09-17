0c6ba390b7076dcfc56e1401cd2497b82d79b718
abdullahi-7 <abdullahi.abdi@protonmail.com>
2021-09-17 14:11:54 +0300

    i can not see in screen correct answer

--- is_it_even.py
+++ is_it_even.py
@@ -2,10 +2,10 @@ import time
 def even():
     vari = []
     var = int(input("pick a number"))
-    if var // 2 == 0:
+    if var % 2 == 0:
         vari.append(var)
-        print("yes even")
     else:
         print(f"not even{var}")
-
-even()
+    return vari
+    
+print(even())
